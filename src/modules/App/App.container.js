389b6a7c830f7c5d96295e4a410736c4d1725147
Neil Russell <neilrussell6@gmail.com>
2019-03-15 12:49:21 +0200

    chore: fix linting issues.

--- src/modules/App/App.container.js
+++ src/modules/App/App.container.js
@@ -2,10 +2,8 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
 import App from './App'
-import selector from './App.selector'
 
-const mapStateToProps = selector
-
+const mapStateToProps = () => null
 const mapDispatchToProps = null
 
 export default withRouter(connect(
--- src/modules/App/App.js
+++ src/modules/App/App.js
@@ -1,28 +1,15 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import './App.scss'
-import { ROUTE_INVENTORY } from '../Inventory/Inventory.reducer'
 import Inventory from '../Inventory'
 
-const App = ({
-  route,
-}) => {
-  const views = {
-    [ROUTE_INVENTORY]: <Inventory />,
-  }
-  return (
-    <div className="App">
-      <main className="App__main">
-        {views[route]}
-      </main>
-    </div>
-  )
-}
-
-App.propTypes = {
-  route: PropTypes.string.isRequired,
-}
+const App = () => (
+  <div className="App">
+    <main className="App__main">
+      <Inventory />
+    </main>
+  </div>
+)
 
 export default App
 
