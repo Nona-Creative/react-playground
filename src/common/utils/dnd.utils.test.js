9c0315469c7fe67ccccf701d29fcee8a062e9c01
Neil Russell <neilrussell6@gmail.com>
2019-03-15 10:15:09 +0200

    test: add tests for calculateMaxHP.

--- src/common/utils/dnd.utils.test.js
+++ src/common/utils/dnd.utils.test.js
@@ -5,7 +5,44 @@ import * as SUT from './dnd.utils'
 
 describe('DnD Utils', () => {
   describe('calculateMaxHP', () => {
-    // TODO: should return expected value for max HP calculation for class (${charClass}) at level (${charLevel}) with constitution (${charConstitution})
+    parametrize([
+      ['barbarian', 15, 13, 125],
+      ['rogue', 10, 18, 93],
+      ['wizard', 15, 18, 122],
+      ['wizard', 16,  1, 16],
+      ['barbarian', 15,  7, 80],
+      ['warlock', 15,  3, 18],
+      ['ranger', 14,  1, 18],
+      ['warlock',  3, 14, 24],
+      ['druid',  3,  4, 9],
+      ['cleric', 11,  5, 25],
+      ['bard', 20, 11, 103],
+      ['barbarian', 11, 13, 93],
+      ['bard',  8, 19, 75],
+      ['bard', 16, 17, 131],
+      ['fighter', 10,  6, 44],
+      ['monk', 10,  2, 13],
+      ['cleric', 14, 17, 115],
+      ['cleric',  6,  5, 15],
+      ['rogue',  7, 13, 45],
+      ['cleric',  4, 14, 31],
+      ['rogue', 19, 15, 136],
+      ['paladin', 13, 13, 95],
+      ['cleric', 13, 15, 94],
+      ['bard',  8,  5, 19],
+      ['monk', 20, 11, 103],
+      ['barbarian',  8, 20, 101],
+      ['monk',  1,  4, 5],
+      ['bard',  5, 17, 43],
+      ['monk', 18,  7, 57],
+      ['wizard', 17,  5, 19],
+    ], (charClass, charLevel, charConstitution, expected) => {
+      it(`should return expected value for max HP calculation for class (${charClass}) at level (${charLevel}) with constitution (${charConstitution})`, () => {
+        // when ... we calculate the max HP for the provided character class, level and constitution
+        // then ... should return expected values
+        assert.strictEqual(SUT.calculateMaxHP(charClass, charLevel, charConstitution), expected)
+      })
+    })
   })
 
   describe('calculateMaxWeaponDamage', () => {
