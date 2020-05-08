d977cdff663b7507e45d8bf667e82402eda74880
Neil Russell <neilrussell6@gmail.com>
2020-05-08 12:23:01 +0200

    refactor: fix eslint, make as close to create-react-app as possible

--- src/modules/App/App.test.js
+++ src/modules/App/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+
 import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+describe('modules/App', () => {
+  describe('render', () => {
+    it('should render successfully', () => {
+      const { getByText } = render(<App />)
+      const linkElement = getByText(/REACT Playground/i)
+      expect(linkElement).toBeInTheDocument()
+    })
+  })
 })
