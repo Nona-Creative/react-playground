54df43d98f677711285a2294644715ea394bf948
Neil Russell <neilrussell6@gmail.com>
2020-05-10 14:43:58 +0200

    chore: move branch docs into separate directory

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+# import .env
+set -o allexport
+[[ -f .env ]] && source .env
+set +o allexport
+
 # ------------------------------------------------------
 # generate
 # ------------------------------------------------------
 
 function generate() {
   local BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-  local DESTINATION_PATH="docs/${BRANCH_NAME}.tpl.md"
+  local DIR=${BRANCH_DOCS_DIRECTORY:-docs}
+  local DESTINATION_PATH="${DIR}/${BRANCH_NAME}.tpl.md"
   if [[ ! -f "$DESTINATION_PATH" ]]; then
     touch $DESTINATION_PATH
   fi
@@ -18,7 +24,8 @@ function generate() {
 
 function render() {
   local BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-  local SOURCE_PATH="docs/${BRANCH_NAME}.tpl.md"
+  local DIR=${BRANCH_DOCS_DIRECTORY:-docs}
+  local SOURCE_PATH="${DIR}/${BRANCH_NAME}.tpl.md"
   local DESTINATION_PATH="${SOURCE_PATH/.tpl.md/.md}"
   if test -f "$SOURCE_PATH"; then
     node scripts/docs.js render $SOURCE_PATH > $DESTINATION_PATH
